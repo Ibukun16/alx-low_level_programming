2551180e85351a5b17122ec3ed54a4df38ff7587
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-07 19:42:06 -0700

    My printf command code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print like
+ *
+ * Description: 'My code'
+ *
+ * Return: Good result can be achieved
+ */
+
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
