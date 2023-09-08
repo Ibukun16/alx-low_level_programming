525d705843b39ae9c521c04b9fb335b9bed87b5a
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-07 20:42:39 -0700

    Finding size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdin.h>
+/**
+ * main - This program prints the size of files
+ *
+ * Return: The result is as seen
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	return(0);
+}
