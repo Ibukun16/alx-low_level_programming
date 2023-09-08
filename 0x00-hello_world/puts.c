26cb9f6a79e7cc5fa9ee80a8243614c0b5a68c03
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-07 18:21:02 -0700

    My first programming task in C

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programming in c
+ * Return: zero successful always
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
