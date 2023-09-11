2e6a645dba13479500ac6ba3622664d2490761a1
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-11 11:13:19 -0700

    My third commit is alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet game
+ * print alphabet in lower case followed by a line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char aplhabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
