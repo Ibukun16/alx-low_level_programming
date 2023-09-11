d801a8151cafef0de3896fb7977caaa9922712c0
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-11 15:46:53 -0700

    Additional task one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print number combinations without repitition
+ * Return: Being successful is the goal
+ */
+int main(void)
+{
+	dig1, dig2;
+
+	for (dig1 = 0; dig1 < 9; dig++)
+	{
+		for (dig2 = 0; dig2 < 10; dig++)
+		{
+			putchar((dig1 % 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 8 && dig2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
