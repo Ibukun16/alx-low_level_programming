5f573e60f6ea3db1e7ad83c41f786436667302ef
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-11 13:54:38 -0700

    sixth commit in variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers
+ * prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * Return:Always successful
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
