57f4e3665e5e535bfcadccc5d21fa4591fafe375
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-11 15:51:50 -0700

    Additional task one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int dig1, dig2;
 
-	for (dig1 = 0; dig1 < 9; dig++)
+	for (dig1 = 0; dig1 < 9; dig1++)
 	{
-		for (dig2 = 0; dig2 < 10; dig++)
+		for (dig2 = 0; dig2 < 10; dig2++)
 		{
 			putchar((dig1 % 10) + '0');
 			putchar((dig2 % 10) + '0');
