b2301cd24c2741c88e84f62dd4ac00a88e1e6535
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-12 15:49:36 -0700

    commit to printng alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the lower case alphabet ten times
+ * @code for printing lower case alphabet ten times
+ * Return: Always 0 (succes).
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alphabet = 'a';
+
+	while (count++ <= 9)
+	{
+		while (alphabet++ <= 'z')
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
