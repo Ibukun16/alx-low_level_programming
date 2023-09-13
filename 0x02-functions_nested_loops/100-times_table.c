54e1ef8e27d413bf612ce6e420bedfaee8724338
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-13 05:18:27 -0700

    Additional task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,51 @@
 #include "main.h"
 /**
  * print_times_table - multiplication tables
- * Description: programme that prints the multiplication
- * table of the input value, starting with 0
- *
- * @n: The value of the number of times table
+ *@n: the paratmeter of interest
+ * Description: programme that prints the multiplication of numbers
  * Return: Return a complete multiplication table
  */
 void print_times_table(int n)
 {
-	int no, row, column;
+	int row_no, col_no, multi;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (row = 1; row <= n; row++)
+		for (row_no = 0; row_no <= n; row_no++)
 		{
-			_putchar('0');
-
-			for (column = 1; column <= n; column++)
+			for (col_no = 0; col_no <= n; col_no++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				no = row * column;
-
-				if (no <= 99)
-					_putchar(' ');
-				if (no <= 9)
-					_putchar(' ');
-
-				if (no >= 100)
+				multi = col_no * row_no;
+				if (col_no == 0)
 				{
-					_putchar((no / 100) + '0');
-					_putchar(((no / 1090)) % 10 + '0');
+					_putchar(multi + '0');
 				}
-				else if (no <= 99 && no >= 10)
+				else if (multi < 10 && col_no != 0)
 				{
-					_putchar((no / 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(multi + '0');
 				}
-				_putchar((no % 10) + '0');
+				else if (multi >= 10 && multi < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((multi / 10) + '0');
+					_putchar((multi % 10) + '0');
+				}
+				else if (multi >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((multi / 100) + '0');
+					_putchar(((multi / 10) % 10) + '0');
+					_putchar((multi % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
 	}
 }
-
-
-
