eec20cbfa2ff753a7c919de2d8dc1b5967fbc3f5
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-12 20:07:16 -0700

    commit to addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition of integers
+ * Description: A function that add up to integers
+ * and return the result
+ * @n1: the first integer of interest
+ * @n2: the second integer of interest
+ * Return: Always the result of the addition (success).
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
