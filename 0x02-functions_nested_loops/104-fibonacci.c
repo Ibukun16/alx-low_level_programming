c2a86be4ed1f4ac4152eb060cdebaeea3ede7306
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-14 04:29:53 -0700

    commit the last fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print the first 98 fibonacci sequence
+ * Return: Return success(0).
+ */
+
+int main(void)
+{
+	int counter = 3;
+
+	float m = 1;
+	float n = m + 1;
+	float sum = m + n;
+
+	printf("%.0f, ", m);
+	printf("%.0f, ", n);
+	while (counter < 98)
+	{
+		counter++;
+		printf("%.0f, ", sum);
+		m = n;
+		n = sum;
+		sum = m + n;
+		if (counter <= 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
