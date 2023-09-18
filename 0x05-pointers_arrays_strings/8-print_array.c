c67bdb054bc93c003addac1ff837f59568c2b36f
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-18 16:20:56 -0700

    commit to print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * and terminate on a new line
+ * @n: number of elements
+ * @a: pointer
+ *
+ * Return: Always a success.
+ */
+void print_array(int *a, int n)
+{
+	int l = 0;
+
+	while (l < n)
+	{
+		printf("%d", a[n]);
+		l++;
+		if (l < n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
