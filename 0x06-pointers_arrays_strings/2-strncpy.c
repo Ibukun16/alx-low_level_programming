692a7ace6cff8b26955942c8862c963b13210306
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-21 14:13:48 -0700

    commit to copy string in pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+/**
+ * _strncpy - copies a string and work
+ * exactly like strncpy in standard library
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The number of times to copy
+ *
+ * Return: char value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (src[a] != '\0' && a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
