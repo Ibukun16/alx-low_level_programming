63ac556e251a7c084e6c031612c4d2cb246f561b
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-24 09:25:18 -0700

    commit to print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,66 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - A function that prints a buffer
- * @b: The buffer to be printed
- * @size: The number of bytes to be printed from the buffer
+ * print_line - A function that prints line of a buffer
+ * @P: The pointer to the buffer to be printed
+ * @byte: Number of byte to be printed from the buffer
  * Description: The function prints a buffer 10 bytes at a time
  *	starting with the byte position, them showing the hex content,
  *	then displaying printable characters.
+ * @l: the line number of the buffer
+ * Return: Success (0)
+ */
+void print_line(char *P, int byte, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= byte)
+			printf("%02x", P[l * 10 + j]);
+		else
+			printf(" ");
+
+		if (j % 2)
+			putchar(' ');
+	}
+
+	for (k = 0; k  <= byte; k++)
+	{
+		if (P[l * 10 + k] > 31 && P[l * 10 + k] < 127)
+			putchar(P[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - A function that prints buffer
+ * @b: The pointer to the buffer to be printed
+ * @size: size of the buffer.
+ *
+ * Return: Success (0)
  */
 void print_buffer(char *b, int size)
 {
-	int byte, idx;
+	int idx;
 
-	for (byte = 0; byte < size; byte += 10)
+	for (idx = 0; idx <= (size - 1) / 10 && size; idx++)
 	{
-		printf("%08x: ", byte);
-
-		for (idx = 0; idx < 10; idx++)
+		printf("%08x: ", idx * 10);
+		if (idx < size / 10)
 		{
-			if ((idx + byte) >= size)
-				printf(" ");
-			else
-				printf("%02x", *(b + idx + byte));
-			if ((idx % 2) != 0 && idx != 0)
-				printf(" ");
+			print_line(b, 9, idx);
 		}
-
-		for (idx = 0; idx < 10; idx++)
+		else
 		{
-			if ((idx + byte) >= size)
-				break;
-
-			else if (*(b + idx + byte) >= 31 &&
-				 *(b + idx + byte) <= 126)
-				printf("%c", *(b + idx + byte));
-
-			else
-				printf(".");
+			print_line(b, size % 10 - 1, idx);
 		}
-
-		if (byte >= size)
-			continue;
-		printf("\n");
+		putchar('\n');
 	}
-	if (size <= 0)
-		printf("\n");
+
+	if (size == 0)
+	{
+		putchar('\n');
+	}
 }
