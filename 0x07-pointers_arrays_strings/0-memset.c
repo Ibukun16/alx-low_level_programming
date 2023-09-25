2907a59532bce26d1acb6c98d8fe85751ed58744
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-25 10:44:10 -0700

    commit to print constant value in memory space

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memset - A function that fills memory with a constant byte
+ * @s: memory area to be filled 
+ * @b: The constant to be filled in the memory space
+ * @n: The number of memory spaces to be filled 
+ *
+ * Return: success (0).
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The folder contains more codes on pointers, arrays and strings. All you need to kow about pointers, arrays and strings and extensively covered in the codes saved in this folder.
