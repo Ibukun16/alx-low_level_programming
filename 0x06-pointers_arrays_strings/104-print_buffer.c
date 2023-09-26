f649febbcbf46ef1f6937674a2e9cdba7205a619
Ibukun16 <ib.agunbiade18@gmail.com>
2023-09-26 03:41:55 -0700

    commit to print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,48 +11,30 @@
 
 void print_line(char *p, int t, int l)
 {
-int j, k;
-for (j = 0; j <= 9; j++)
-{
-if (j <= t)
-printf("%02x", p[l * 10 + j]);
-else
-printf(" ");
-if (j % 2)
-putchar(' ');
-}
-for (k = 0; k <= t; k++)
-{
-if (p[l * 10 + k] > 31 && p[l * 10 + k] < 127)
-putchar(p[l * 10 + k]);
-else
-putchar('.');
-}
+	int j, k;
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= t)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf(" ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= t; k++)
+	{
+		if (p[l * 10 + k] > 31 && p[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
 }
 
-/**
- * print_buffer - A function that prints buffer
+
+r - A function that prints buffer
  * @b: The pointer to the buffer to be printed
  * @size: size of the buffer.
  *
  * Return: void
  */
-void print_buffer(char *b, int size)
-{
-int i;
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-{
-printf("%08x: ", i * 10);
-if (i < size / 10)
-{
-print_line(b, 9, i);
-}
-else
-{
-print_line(b, size % 10 - 1, i);
-}
-putchar('\n');
-}
-if (size == 0)
-putchar('\n');
-}
+
