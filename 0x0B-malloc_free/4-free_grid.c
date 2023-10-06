57ba3c69d6da72acff213490ac5a1904d799e2de
Ibukun16 <ib.agunbiade18@gmail.com>
2023-10-05 17:23:58 -0700

    commit to free up allocated grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - a function that frees a 2 dimensional grid
+ * previously created by your alloc_grid function.
+ * @grid: expected to be freed
+ * @height: height of grid
+ *
+ * Return: Always (0)
+ */
+void free_grid(int **grid, int height)
+{
+	int f = 0;
+
+	if (grid == NULL || height == 0)
+	{
+		return;
+	}
+
+	while (f < height)
+	{
+		free(grid[f]);
+	}
+
+	free(grid);
+}
