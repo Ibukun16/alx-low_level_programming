341d596d4b1a7bc4169fee20b84588296c839603
Ibukun Agunbiade <ib.agunbiade18@gmail.com>
2023-10-28 15:15:10 -0700

    commit to print the argument on the command line

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,16 +4,16 @@
  * main - A function that prints all the arguments it receives
  * @argv: An array containing argument c
  * @argc: Count of the argument
- * @i: integer we initially come abruptly to stop worker
- * set 
+ * 
  * Return: Content of the array.
  */
 int main(int argc, char *argv[])
 {
-	int i;
-       for(i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
+	int i = 0;
+       while (i < argc)
+       {
+	       printf("%s\n", argv[i]);
+	       i++;
+       }
+       return (0);
 }
