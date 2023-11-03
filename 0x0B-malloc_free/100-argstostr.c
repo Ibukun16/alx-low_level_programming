e4b2a31411cb74638e4988124f3abcc6d570f521
Ibukun Agunbiade <ib.agunbiade18@gmail.com>
2023-11-03 00:46:02 -0700

    commit to print concatenate argument on command line

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *argstostr - A function that concatenate all arguments
+ * of a program
+ * @ac: argument count
+ * @av: array of the argument
+ *
+ * Return: The concatenated argument
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, a, b, len = 0, siz = 0;
+	char *arg;
+
+	if (ac == 0 || av == NULL)
+	return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+			len++;
+		}
+	}
+	arg = malloc((sizeof(char) * len) + 1);
+	if (arg == NULL)
+		return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			arg[siz] = av[a][b];
+			siz++;
+		}
+		arg[siz++] = '\n';
+	}
+	arg[siz] = '\0';
+	return (arg);
+}
