6054f9014b9537e034b4a1af5ddc70f7a6012ccd
Ibukun Agunbiade <ib.agunbiade18@gmail.com>
2023-12-08 17:45:34 +0100

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
-
-int main(int argc __attribute__((unused)), char *argv[]);
-int main(int argc, char *argv[] __attribute__((unused)));
-int main(int argc, char *argv[]);
-
-#endif /** MAIN_FILE **/
