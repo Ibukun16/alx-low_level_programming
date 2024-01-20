95281a7bb65cfaa0e2e52e2d6b66ac65287d1d72
Ibukun16 <ib.agunbiade18@gmail.com>
2024-01-19 21:14:44 -0800

    print the name of a program using argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,7 @@
  * main - A program that prints its own name.
  * argc: The count of the number of argument supplied to the command line.
  * argv: An array vector of the argument strings.
+ *
  * Return: 0 for success.
  */
 int main(int argc, char *argv[])
