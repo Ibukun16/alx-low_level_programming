4c2496f2ce1e452b9fab94614dd4b7d7a50bd247
Ibukun16 <ib.agunbiade18@gmail.com>
2024-01-19 21:17:08 -0800

    print the number of arguments supplied to the command line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,6 +5,7 @@
  * to the commandline.
  * argc: The count of the number of arguments.
  * argv: A vector array containing the argument strings.
+ *
  * Return: 0 for success.
  */
 int main(int argc, __attribute__((unused)) char *argv[])
