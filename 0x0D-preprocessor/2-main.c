12df2ecff274cf762fa7dfae9d809262ab1f5525
Ibukun16 <ib.agunbiade18@gmail.com>
2024-01-25 00:47:13 -0800

    A programme that prints the name of the file it compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - A programme that print the name of the library
+ * it is compiled from.
+ * @void: Indicates no value.
+ *
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
