617b7f50024012805a5f0e7af4cb54073a8d44bb
Ibukun16 <ib.agunbiade18@gmail.com>
2024-01-26 02:45:07 -0800

    commit to print any thing

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - A function that print all argument passed to it.
+ * @format: The format of each of the entry on the command line.
+ *
+ * Return: 0 for success.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list varg;
+	int i, j;
+	char *strn;
+	char *separator = ", ";
+
+	va_start(varg, format);
+
+	i = 0, j = 0;
+	while (format && format[i])
+		i++;
+	while (format && format[j])
+	{
+		if (j == (i - 1))
+			separator = "";
+		switch (format[j])
+		{
+			case 'c':
+				printf("%c%s", va_arg(varg, int), separator);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(varg, int), separator);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(varg, double), separator);
+				break;
+			case 's':
+				strn = va_arg(varg, char *);
+				if (strn)
+					printf("%s%s", strn, separator);
+				else
+					printf("(nil)");
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(varg);
+}
