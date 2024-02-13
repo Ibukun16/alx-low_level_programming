40a7750e1dc57ad1c78fab353056c74a55535a32
Ibukun16 <ib.agunbiade18@gmail.com>
2024-02-13 10:04:51 -0800

    print binary representation of numbers

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 /**
  * _putchar - A function that prints character to standard output
  * @c: The character to print
@@ -6,7 +6,7 @@
  * Return: On success -1.
  * On error, -1 is returned, and erno is set apprpriately.
  */
-int _purchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
