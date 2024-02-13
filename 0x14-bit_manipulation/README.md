9c28f6c5e62a6cf16f5a2ba4c4c0f51e8890fee7
Ibukun16 <ib.agunbiade18@gmail.com>
2024-02-13 07:20:45 -0800

    convert a binary to unsigned integer

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This folder contains practise assignments on bit manipulation
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /** MAIN_HEADER_FILE **/
