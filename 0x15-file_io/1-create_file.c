b8f428dc92ea5864eb63289841c85f91a11dae81
Ibukun16 <ib.agunbiade18@gmail.com>
2024-02-26 05:36:17 -0800

    commit to create a file and grant the rw permission to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-		l++;
+		for (l = 0; text_content[l]; l++)
+			;
 	}
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(op, text_content, l);
 	if (op == -1 || wrt == -1)
 		return (-1);
