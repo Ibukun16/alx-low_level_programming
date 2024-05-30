11c32caa09d958d68197f2e4f2f79222334176fc
Ibukun16 <ib.agunbiade18@gmail.com>
2024-05-30 14:39:03 -0700

    make Makefile project task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This project isa Technical Interview question base on Makefiles"""
+
+
 def island_perimeter(grid):
     """Return the perimeter of the island represented by the grid.
 
@@ -8,17 +10,17 @@ def island_perimeter(grid):
     Arguments:
         grid(list of integers): Representing an area of land.
     """
-    b = len(grid[0])
-    l = len(grid)
+    breath = len(grid[0])
+    length = len(grid)
     sz = 0
     cell = 0
 
-    for i in range(l):
-        for w in range(b):
-            if grid[i][w] == 1:
+    for i in range(length):
+        for b in range(breath):
+            if grid[i][b] == 1:
                 sz += 1
-                if (w > 0 and grid[i][w - 1] == 1):
+                if (b > 0 and grid[i][b - 1] == 1):
                     cell += 1
-                if (i > 0 and grid[i - 1][w] == 1):
+                if (i > 0 and grid[i - 1][b] == 1):
                     cell += 1
     return sz * 4 - cell * 2
