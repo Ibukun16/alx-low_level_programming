bf95dbdf29a8f950830e401123f7c3f39d824a3a
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-12 07:51:55 -0700

    Update task 0 to implement malloc

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This project directory contains solutions to practise tasks on dynamic and automatic memory allocation
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main(void);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif /** _MAIN_H_ **/
