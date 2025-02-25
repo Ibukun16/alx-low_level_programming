244a5c38578508345e5e0fd176a0c27e568c7e76
Ibukun16 <ib.agunbiade18@gmail.com>
2025-02-25 14:19:02 +0100

    Update changes to project directory 05

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,13 +9,11 @@
  */
 void _puts(char *str)
 {
-	int c = 0;
 
-	while (str[c] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[c]);
-		c++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
-
