36d9c94feeb71b2a021eda13d1f64b1816b4aae7
Ibukun16 <ib.agunbiade18@gmail.com>
2025-02-26 17:56:17 +0100

    Update the correction to project task 2 and return correct result

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - A function that concatenate two strings
+ * @s1: The first string variable
+ * @s2: The second string variable to concatenate
+ *
+ * Return: The concatenated string, else NULL.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int n1 = 0, n2 = 0, count;
+
+	while (*(s1 + n1) != '\0')
+		n1++;
+	while (*(s2 + n2) != '\0')
+		n2++;
+
+	concat = malloc(sizeof(char) * (n1 + n2 + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (count = 0; count < n1; count++)
+		*(concat + count) = *(s1 + count);
+
+	for (count = 0; count < n2;  count++)
+		*(concat + n1 + count) = *(s2 + count);
+
+	*(concat + n1 + n2) = '\0';
+	return (concat);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_HEADER_
 
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 
 #endif /** _MAIN_HEADER_ **/
