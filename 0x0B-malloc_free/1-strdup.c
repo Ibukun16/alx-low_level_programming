b59b71b112ac7005dff9513a5d5858444e5881ce
Ibukun16 <ib.agunbiade18@gmail.com>
2025-02-26 16:44:28 +0100

    Update changes to project task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - A function that returns a pointer to a newly
+ * allocated memory space, which contains copy of a given string.
+ * @str: The string variable to be copied
+ *
+ * Return: Pointer to the newly allocated memory space.
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int size, count;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + size) != '\0')
+		size++;
+
+	ar = malloc(sizeof(char) * size + 1);
+	if (ar == NULL)
+		return (NULL);
+
+	for (count = 0; count < size; count++)
+		*(ar + count) = *(str + count);
+
+	*(ar + size) = '\0';
+	return (ar);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_HEADER_
 #define _MAIN_HEADER_
 
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 
 #endif /** _MAIN_HEADER_ **/
