e94365e327cd9a6760aa2316581838e7f01a36be
Ibukun16 <ib.agunbiade18@gmail.com>
2025-02-28 11:19:04 +0100

    Update and execute the changes made to task 3 to return array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (count = min; count <= max; count++)
+	for (count = 0; count <= max; count++)
 		arr[count] = min++;
 	return (arr);
 }
