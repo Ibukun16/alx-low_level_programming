351d00969764720e40e5b9b2222d0371ca1a2b21
Ibukun16 <ib.agunbiade18@gmail.com>
2025-02-28 11:08:42 +0100

    Update and execute the changes made to task 3 to return array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - A function that creates an array of integers
+ * @min: The minimum value of integer variable
+ * @max: The maximum value of integer variable
+ *
+ * Return: Pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int *arr, count;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * ((max + 1) - min));
+	if (arr == NULL)
+		return (NULL);
+
+	for (count = min; count <= max; count++, min++)
+		arr[count] = min;
+	return (arr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_HEADER_
 
 int _strlen(char *str);
+int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
