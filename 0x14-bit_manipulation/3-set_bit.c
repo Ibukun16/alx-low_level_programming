bcf722a950db4fa7b5eb97180f45a593af45596e
Ibukun16 <ib.agunbiade18@gmail.com>
2025-05-23 19:08:16 +0100

    Update changes made to project tasks

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,6 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * CHAR_BIT))
 		return (-1);
-	*n |= (1UL <<index);
+	*n |= (1UL << index);
 	return (1);
 }
